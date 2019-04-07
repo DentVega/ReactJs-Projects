b2e7a763eccc929b9217eb894682a7b25a3b845b
Brian Dennis Vega Hidalgo <dentvega6@gmail.com>
2019-04-07 11:51:39 -0400

    Proyecto base podcast

---
+++ podcast/src/components/AppBarLogin/AppBarLogin.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import AppBar from "@material-ui/core/es/AppBar/AppBar";
+import Toolbar from "@material-ui/core/es/Toolbar/Toolbar";
+
+class AppBarLogin extends Component {
+    render() {
+        return (
+            <div>
+                <AppBar>
+                    <Toolbar>
+
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
+}
+
+export default AppBarLogin;
