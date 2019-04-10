8fdd55ac4ccc6ffaf8a89b669f14692f43ccc9d5
Brian Dennis Vega Hidalgo <dentvega6@gmail.com>
2019-04-10 08:52:21 -0400

    Modificacion ExampleApiRequest se añadio el metodo Get, Post, Put, Patch, Delete

--- podcast/src/components/exampleApiRequest/ExampleApiRequest.js
+++ podcast/src/components/exampleApiRequest/ExampleApiRequest.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import CircularProgress from "@material-ui/core/es/CircularProgress/CircularProgress";
+import {withStyles} from '@material-ui/core/styles';
 import Button from "@material-ui/core/es/Button/Button";
 
 const styles = theme => ({
     progress: {
         margin: theme.spacing.unit * 2,
+    },
+    button: {
+        margin: theme.spacing.unit,
     },
 });
 
@@ -48,10 +50,10 @@ class ExampleApiRequest extends Component {
             this.setState({
                 loading: false,
                 data: {
-                  info: data.info,
-                  results: [].concat(
-                      this.state.data.results, data.results
-                  )
+                    info: data.info,
+                    results: [].concat(
+                        this.state.data.results, data.results
+                    )
                 },
                 nextPage: this.state.nextPage + 1,
             })
@@ -64,37 +66,86 @@ class ExampleApiRequest extends Component {
 
     };
 
+    fetchGet = async () => {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/todos/1`);
+        const data = await response.json();
+        console.log(data);
+
+        const response1 = await fetch(`https://jsonplaceholder.typicode.com/todos/1`)
+            .then(response => response.json())
+            .then(json => console.log(json));
+
+    };
+
+    fetchPost = async () => {
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+                title: 'foo',
+                body: 'bar',
+                userId: 1
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then(response => response.json())
+            .then(json => console.log(json))
+    };
+
+    fetchPatch() {
+        fetch('https://jsonplaceholder.typicode.com/posts/1', {
+            method: 'PATCH',
+            body: JSON.stringify({
+                title: 'foo'
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then(response => response.json())
+            .then(json => console.log(json))
+    };
+
+    fetchPut = async () => {
+        fetch('https://jsonplaceholder.typicode.com/posts/1', {
+            method: 'PUT',
+            body: JSON.stringify({
+                id: 1,
+                title: 'foo',
+                body: 'bar',
+                userId: 1
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then(response => response.json())
+            .then(json => console.log(json))
+    };
+
+    fetchDelete = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts/1', {
+            method: 'DELETE'
+        })
+    };
+
     render() {
-        const { classes } = this.props;
-        if (this.state.error){
+        const {classes} = this.props;
+        if (this.state.error) {
             return `Error: ${this.state.error.message}`
         }
 
         return (
             <div>
-                <ul className={"row"}>
-                    {
-                        this.state.data.results.map(character => (
-                            <li className={"col-6 col-md-3"} key={character.id}>
-                                <CharacterCard character={character}/>
-                            </li>
-                        ))
-                    }
-                </ul>
-                {
-                    this.state.loading && (
-                        <CircularProgress className={classes.progress}/>
-                    )
-                }
-
-                {
-                    !this.state.loading && (
-                        <Button onClick={() => this.fetchCharacters()}>Load More</Button>
-                    )
-                }
+                <Button onClick={() => this.fetchGet()} variant="contained" color="primary" className={classes.button}>Fetch Get</Button>
+                <Button onClick={() => this.fetchPost()} variant="contained" color="primary" className={classes.button}>Fetch Post</Button>
+                <Button onClick={() => this.fetchPatch()} variant="contained" color="primary" className={classes.button}>Fetch Patch</Button>
+                <Button onClick={() => this.fetchPut()} variant="contained" color="primary" className={classes.button}>Fetch Put</Button>
+                <Button onClick={() => this.fetchDelete()} variant="contained" color="primary" className={classes.button}>Fetch Delete</Button>
             </div>
         );
     }
 }
 
-export default withStyles(styles) (ExampleApiRequest);
+export default withStyles(styles)(ExampleApiRequest);
